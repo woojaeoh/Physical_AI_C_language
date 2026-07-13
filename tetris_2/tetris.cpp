6cf6d395559bb2533669f0dc4acc98550cc6d1db
user <user@LAPTOP-U0CO45L6>
2026-07-13 16:40:13 +0900

    테트리스 내려오면서 키보드로 제어

--- tetris_2/tetris.cpp
+++ tetris_2/tetris.cpp
@@ -3,7 +3,7 @@
 #include<windows.h>
 #include <conio.h>
 
-void gotoxy(int x, int y) {
+void gotoxy(int x, int y) { // 콘솔화면 내에서 x,y좌표로 이동
 
 	COORD Pos;
 	Pos.X = x;
@@ -11,7 +11,7 @@ void gotoxy(int x, int y) {
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
 }
 
-char block[4][4] = {
+char block[4][4] = { // 예제 테트리스 블럭 모양
 	{ 0, 0, 0, 0 },
 	{ 0, 1, 0, 0 },
 	{ 0, 1, 1, 1 },
@@ -37,7 +37,7 @@ void make_block(int xx, int yy) {
 	}
 }
 
-// 블럭 삭제 
+// 블럭 삭제 - 화면에서 공백처리해서 지움 
 void delete_block(int xx, int yy) {
 	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 4; j++) {
@@ -58,22 +58,67 @@ void delete_block(int xx, int yy) {
 }
 
 
-
+// 이동 좌표값 - 직접 ++, -- 처럼 관리 필요
 int x = 0;
 int y = 0;
 
+int count = 0;
 
 
 void main() {
 
-	make_block(x, y);
+	// make_block(x, y); // 초기 블럭 생성
 
-	while (y < 4 and x < 4) {
-		delete_block(x, y);
-		x++;
+
+	//강의 예제
+	while (1) {
 		make_block(x, y);
-		//y++;
-		Sleep(1000);
+
+		while (1) {
+			if (count == 100) {
+				count = 0;
+
+				delete_block(x, y);
+				y++;
+				make_block(x, y);
+
+			}
+
+			//delete_block(x, y);
+			//x++; // 오른쪽으로
+			//y++; // 밑으로
+			//make_block(x, y);
+
+			if (_kbhit()) { // 키보드 입력이 있다면 
+				char key = _getch();
+				if (key == 'w') { // w: up key
+					delete_block(x, y);
+					y--;
+					make_block(x, y);
+				}
+				else if (key == 's') { // s: down key
+					delete_block(x, y);
+					y++;
+					make_block(x, y);
+				}
+				else if (key == 'a') { // a: left key
+					delete_block(x, y);
+					x--;
+					make_block(x, y);
+				}
+				else if (key == 'd') { // d: right key
+					delete_block(x, y);
+					x++;
+					make_block(x, y);
+				}
+			}
+			count++;
+			Sleep(10);
+		}
 	}
+
+
+
+	
 }
 
